513f961e4fe5b9fd37a7205321c23e4ef62f80f5
Sylvion Finisie <sylvionud@gmail.com>
2023-02-12 12:36:13 -0500

    Update main.py
    I had to remote information.

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# Do not modify these lines
-__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
-__human_name__ = 'arguments'
-
-# Add your code after this line
-
 G = 6.67430 * (10**-11)
 
 def greet(name, greeting_template="Hello, <name>!"):
